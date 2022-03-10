9d65a51bc7c889f9592cab047d7277e587dbb720
hector <hectornuno1994@gmail.com>
2022-03-09 17:19:37 -0800

    finishes excercise

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,2 +1,6 @@
 class ActorsController < ApplicationController
+  def benedict_method
+    actor = Actor.find(6)
+    render json: actor.as_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "/benedict" => "actors#benedict_method"
 end
