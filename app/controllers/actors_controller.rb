779666972c0fc3cac81e102e4c52d0447965a5e1
hector <hectornuno1994@gmail.com>
2022-03-17 15:56:14 -0700

    updates actors controller

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,19 +1,17 @@
 class ActorsController < ApplicationController
   def index
-    actor = Actor.all
-    @actor = actor
+    @actors = Actor.all
     render template: "actors/index"
   end
 
   def create
-    actor = Actor.create(
+    @actor = Actor.create(
       first_name: params["first_name"],
       last_name: params["last_name"],
       known_for: params["known_for"],
       age: params["age"],
       gender: params["gender"],
     )
-    @actor = actor
     render template: "actors/show"
   end
 
@@ -25,20 +23,20 @@ class ActorsController < ApplicationController
 
   def update
     actor_id = params["id"]
-    actor = Actor.find(actor_id)
-    actor.first_name = params["first_name"] || actor.first_name
-    actor.last_name = params["last_name"] || actor.last_name
-    actor.known_for = params["known_for"] || actor.known_for
-    actor.age = params["age"] || actor.age
-    actor.gender = params["gender"] || actor.gender
-    actor.save
-    render json: actor.as_json
+    @actor = Actor.find(actor_id)
+    @actor.first_name = params["first_name"] || @actor.first_name
+    @actor.last_name = params["last_name"] || @actor.last_name
+    @actor.known_for = params["known_for"] || @actor.known_for
+    @actor.age = params["age"] || @actor.age
+    @actor.gender = params["gender"] || @actor.gender
+    @actor.save
+    render template: "actors/show"
   end
 
   def destroy
     actor_id = params["id"]
-    actor = Actor.find(actor_id)
-    actor.destroy
+    @actor = Actor.find(actor_id)
+    @actor.destroy
     render json: { message: "Actor deleted" }
   end
 end
