3ac29e289339fa3d96e16ffc2eb3481cd981256c
hector <hectornuno1994@gmail.com>
2022-03-24 19:28:44 -0700

    adds associations between MovieGenre, Movie, and Genre models

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,4 +3,6 @@ class Movie < ApplicationRecord
   # validates :director, presence
   # validates :year, length: { minimum: 4 }
   has_many :actors #returns array of many actors
+  has_many :movie_genres
+  has_many :genres, through: :movie_genres
 end
