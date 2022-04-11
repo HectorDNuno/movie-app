bdc495fccf70471565e721fb6d7c705dac3451f9
hector <hectornuno1994@gmail.com>
2022-04-11 16:55:03 -0700

    adds ability to see all movies

--- public/index.js
+++ public/index.js
@@ -1,6 +1,6 @@
-/* global axios */
+// /* global axios */
 
-axios.get("http://localhost:3000/actors.json").then(function (response) {
-  var actors = response.data;
-  console.log(actors);
-});
+// axios.get("http://localhost:3000/actors.json").then(function (response) {
+//   var actors = response.data;
+//   console.log(actors);
+// });
--- public/moviesIndex.js
+++ public/moviesIndex.js
@@ -1,6 +1,6 @@
-/* global axios */
+// /* global axios */
 
-axios.get("http://localhost:3000/movies.json").then(function (response) {
-  var actors = response.data;
-  console.log(actors);
-});
+// axios.get("http://localhost:3000/movies.json").then(function (response) {
+//   var movies = response.data;
+//   console.log(movies);
+// });
